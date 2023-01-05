c43167c7502f18d73e0fd2e591eb45a2231e5d84
Anh Kiệt <igg.anhkiet1@gmail.com>
2023-01-05 20:49:41 +0700

    initical commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'Login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Sweetly 22',
+      home: Login(),
+    );
+  }
+}
