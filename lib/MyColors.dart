080ea4e120280fba31bd81762f44260769becda9
Anh Kiệt <igg.anhkiet1@gmail.com>
2023-01-07 19:04:40 +0700

    change colors

--- lib/MyColors.dart
+++ lib/MyColors.dart
@@ -2,9 +2,12 @@
 import 'dart:ui';
 
 class MyColors{
-  static const Color color1 = Color(0xff6B728E);
-  static const Color color2 = Color(0xff50577A);
-  static const Color color3 = Color(0xff474E68);
-  static const Color color4 = Color(0xff404258);
-  // helo be Kiet
+  // static const Color color1 = Color(0xff6B728E);
+  // static const Color color2 = Color(0xff50577A);
+  // static const Color color3 = Color(0xff474E68);
+  // static const Color color4 = Color(0xff404258);
+  static Color color1 = const Color(0xffCBEDD5);
+  static Color color2 = const Color(0xff97DECE);
+  static Color color3 = const Color(0xff62B6B7);
+  static Color color4 = const Color(0xff439A97);
 }
